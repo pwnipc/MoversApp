077b1e2c902c725d45a234ae3296d264ded8d6b8
carlos kipkoech <carlos.kipkoech@student.moringaschool.com>
2022-01-19 06:30:29 +0300

    add calendar

--- app/src/main/java/com/movers/app/MainActivity.java
+++ app/src/main/java/com/movers/app/MainActivity.java
@@ -2,11 +2,20 @@ package com.movers.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     // Initialize Variables
    @BindView(R.id.buttonOpenDialog)
     Button mButtonOpenDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mButtonOpenDialog.setOnClickListener(this);
     }
 
+
+
     @Override
     public void onClick(View view) {
 
@@ -35,8 +47,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //create new BottomSheetDialog
             BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
             bottomSheetDialog.setContentView(R.layout.choose_inventory_bottom_sheet_dialog);
+            bottomSheetDialog.setCanceledOnTouchOutside(false);
+
+
+
+            // initialize and Assign Variables
+            AutoCompleteTextView mInventorySize = bottomSheetDialog.findViewById(R.id.autoCompleteTextViewInventorySize);
+            AutoCompleteTextView mDateAndTime = bottomSheetDialog.findViewById(R.id.autoCompleteTextViewDate);
+            Button mButtonBook = bottomSheetDialog.findViewById(R.id.buttonBook);
+            mDateAndTime.setInputType(InputType.TYPE_NULL);
+
+            mButtonBook.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Open Summary Activity Dialog
+                }
+            });
+            mDateAndTime.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showDateDialog(mDateAndTime);
+                }
+            });
+
+
+            //show bottomSheetDialog
+            bottomSheetDialog.show();
 
         }
 
+        }
+
+
+    private void showDateDialog(AutoCompleteTextView mAutoCompleteTextViewDate) {
+
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener datePickerDialog = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(calendar.YEAR,i);
+                calendar.set(calendar.MONTH,i1);
+                calendar.set(calendar.DAY_OF_MONTH,i2);
+
+                TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+
+                        calendar.set(calendar.HOUR_OF_DAY,hourOfDay);
+                        calendar.set(calendar.MINUTE,minute);
+
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd HH:mm");
+                        mAutoCompleteTextViewDate.setText(simpleDateFormat.format(calendar.getTime()));
+
+
+
+                    }
+                };
+
+                new TimePickerDialog(MainActivity.this,onTimeSetListener,calendar.get(calendar.HOUR_OF_DAY),calendar.get(calendar.MINUTE),false).show();
+
+            }
+        };
+
+        new DatePickerDialog(MainActivity.this,datePickerDialog,calendar.get(calendar.YEAR),calendar.get(calendar.MONTH),calendar.get(calendar.DAY_OF_MONTH)).show();
     }
 }
