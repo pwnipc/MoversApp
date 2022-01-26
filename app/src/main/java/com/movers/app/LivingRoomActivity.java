c84064fcad76470286c60eb87fce5cf375ab6a74
Ckhajira <christopher.khajira@student.moringaschool.com>
2022-01-26 14:41:41 +0300

    This commit adds the Bedroom and livingroom activity

---
+++ app/src/main/java/com/movers/app/LivingRoomActivity.java
@@ -0,0 +1,14 @@
+package com.movers.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LivingRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_living_room);
+    }
+}
