d8ff0d0513ea4452d0e5f84ef0921c396af73aff
Ckhajira <christopher.khajira@student.moringaschool.com>
2022-02-01 19:28:30 +0300

    This commit initializes the recycler view

--- app/src/main/java/com/movers/app/MoversActivity.java
+++ app/src/main/java/com/movers/app/MoversActivity.java
@@ -1,14 +1,16 @@
 package com.movers.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MoversActivity extends AppCompatActivity {
-
+    private RecyclerView moversRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movers);
+        moversRecyclerView = findViewById(R.id.moversRecyclerView);
     }
 }
