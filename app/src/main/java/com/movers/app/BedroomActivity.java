8924fa9144af99febae300e3fb99e7799c6edd41
Chal13W1zz <charles.muthii@student.moringaschool.com>
2022-02-01 07:34:13 +0300

    add badroom, livingroom and kitchen activities backend logic

--- app/src/main/java/com/movers/app/BedroomActivity.java
+++ app/src/main/java/com/movers/app/BedroomActivity.java
@@ -2,13 +2,59 @@ package com.movers.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class BedroomActivity extends AppCompatActivity {
+    Button submitBedroom;
+    CheckBox bed, drawer, babyCot, chair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bedroom);
+        submitBedroom = findViewById(R.id.submitBedroomButton);
+        addListenerOnButtonClick();
+    }
+
+    public void addListenerOnButtonClick(){
+        bed = (CheckBox) findViewById(R.id.bedCheckBox);
+        drawer = (CheckBox) findViewById(R.id.drawerCheckBox);
+        babyCot = (CheckBox) findViewById(R.id.babyCotCheckBox);
+        chair = findViewById(R.id.chairCheckBox);
+
+        submitBedroom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int totalCost =0;
+                StringBuilder result=new StringBuilder();
+                result.append("Selected Items:");
+
+                if(bed.isChecked()){
+                    result.append("bed : 1000");
+                    totalCost += 1000;
+                }else if(drawer.isChecked()){
+                    result.append("drawer : 500");
+                    totalCost += 500;
+                }else if(babyCot.isChecked()){
+                    result.append("Baby Cot : 500");
+                    totalCost += 500;
+                }else if (chair.isChecked()){
+                    result.append("Microwave : 500");
+                    totalCost += 500;
+                }else if(!bed.isChecked() && !drawer.isChecked() && !babyCot.isChecked() && !chair.isChecked()){
+                    Toast.makeText(BedroomActivity.this,"Please select an item to move",Toast.LENGTH_LONG).show();
+                }
+                Toast.makeText(BedroomActivity.this, "Inventory added successfully",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(BedroomActivity.this, Inventory.class);
+                intent.putExtra("BedroomCost",totalCost);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/movers/app/LivingRoomActivity.java
+++ app/src/main/java/com/movers/app/LivingRoomActivity.java
@@ -2,13 +2,59 @@ package com.movers.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class LivingRoomActivity extends AppCompatActivity {
+    Button submitLivingRoom; 
+    CheckBox tv, sofa, table, woofer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_living_room);
+        addListenerOnButtonClick();
+        submitLivingRoom = (Button) findViewById(R.id.submitLivingRoomButton);
+    }
+
+    public void addListenerOnButtonClick(){
+        tv = (CheckBox) findViewById(R.id.tvCheckBox);
+        sofa = (CheckBox) findViewById(R.id.sofaCheckBox);
+        table = (CheckBox) findViewById(R.id.tableCheckBox);
+        woofer = findViewById(R.id.wooferCheckBox);
+
+        submitLivingRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int totalCost =0;
+                StringBuilder result=new StringBuilder();
+                result.append("Selected Items:");
+
+                if(tv.isChecked()){
+                    result.append("Television : 1000");
+                    totalCost += 1000;
+                }else if(sofa.isChecked()){
+                    result.append("Sofa : 1000");
+                    totalCost += 1000;
+                }else if(table.isChecked()){
+                    result.append("Table : 1000");
+                    totalCost += 1000;
+                }else if (woofer.isChecked()){
+                    result.append("Woofer : 500");
+                    totalCost += 500;
+                }else if(!tv.isChecked() && !sofa.isChecked() && !table.isChecked() && !woofer.isChecked()){
+                    Toast.makeText(LivingRoomActivity.this,"Please select an item to move",Toast.LENGTH_LONG).show();
+                }
+                Toast.makeText(LivingRoomActivity.this, "Inventory added successfully",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(LivingRoomActivity.this, Inventory.class);
+                intent.putExtra("LivingRoomCost",totalCost);
+                startActivity(intent);
+
+            }
+        });
     }
 }
