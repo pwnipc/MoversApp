8e3dd4a577d798c393ac360464b761125a063a9e
Chal13W1zz <charles.muthii@student.moringaschool.com>
2022-02-04 07:16:22 +0300

    add proper activity redirection

--- app/src/main/java/com/movers/app/CreateAccountActivity.java
+++ app/src/main/java/com/movers/app/CreateAccountActivity.java
@@ -48,7 +48,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     public void onClick(View view) {
 
         if (view == mLoginTextView) {
-            Intent intent = new Intent(CreateAccountActivity.this, MapsActivity.class);
+            Intent intent = new Intent(CreateAccountActivity.this, MoversActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
             finish();
@@ -89,7 +89,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 final FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    Intent intent = new Intent(CreateAccountActivity.this, MapsActivity.class);
+                    Intent intent = new Intent(CreateAccountActivity.this, MoversActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                     finish();
